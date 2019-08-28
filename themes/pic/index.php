401d550d4e994c433c105bc83c14b5670fbdeea7
ambientos <ant.grakhov@gmail.com>
2019-08-28 18:34:52 +0500

    Add default page template

--- themes/pic/index.php
+++ themes/pic/index.php
@@ -1,5 +1,11 @@
+<?php /* Template Name: Default Page */ ?>
+
 <?php get_header(); ?>
 
-<?php the_post(); the_content(); ?>
+<?php title_header( get_the_title() ); ?>
+
+<section class="widget container">
+	<?php the_post(); the_content(); ?>
+</section>
 
 <?php get_footer(); ?>
